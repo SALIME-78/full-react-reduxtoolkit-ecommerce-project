648de9fe0428159c04ffe14c88e840ff2bc1a1b3
SALIME-78 <h.salim78.dz@gmail.com>
2024-12-23 01:01:50 +0100

    starting react ecommerce project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+    <h1 className='text-2xl font-bold text-red-600'>Ecommer Reat App</h1>
+    </div>
+  )
+}
+
+export default App
